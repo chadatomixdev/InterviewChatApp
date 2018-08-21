2b5992bd47f2a8712b57df6ed45caf033c2b9473
Chad Bonthuys <chad@freshive.co.za>
2018-08-21 21:55:49 +0200

    Initial Messaging Service Code

---
+++ Messenger/Services/MessagingService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+
+namespace Messenger.Services
+{
+    [Service]
+    [IntentFilter(new String[] { "com.xamarin.MessagingService" })]
+    public class MessagingService : Service
+    {
+        #region Properties
+
+        public IBinder Binder { get; private set; }
+
+        #endregion
+
+        public MessagingService()
+        {
+        }
+
+        public override IBinder OnBind(Intent intent)
+        {
+            //this.Binder = new  TODO Create Binder
+            return Binder;
+        }
+
+        public override bool OnUnbind(Intent intent)
+        {
+            return base.OnUnbind(intent);
+        }
+
+        public override void OnDestroy()
+        {
+            Binder = null;
+            base.OnDestroy();
+        }
+
+    }
+}
---
+++ Messenger/Services/MessagingServiceConnection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Android.Content;
+using Android.OS;
+
+namespace Messenger.Services
+{
+    public class MessagingServiceConnection : Java.Lang.Object, IServiceConnection
+    {
+        #region Properties
+
+        public bool IsConnected { get; private set; }
+        MainActivity mainActivity;
+
+        #endregion
+
+        public MessagingServiceConnection(MainActivity activity)
+        {
+            IsConnected = false;
+            mainActivity = activity;
+        }
+
+        public void OnServiceConnected(ComponentName name, IBinder service)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnServiceDisconnected(ComponentName name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
